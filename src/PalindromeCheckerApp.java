37f17868db5fe7bd9ffd60303d6ca4675651c4f5
itss-omkar <mishraomkarnarayan@gmail.com>
2026-02-27 16:33:07 +0530

    UC7:Deque Based palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,24 +2,21 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         // Original string
-        String original = "level";
+        String original = "racecar";
 
-        // Create Stack and Queue
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-        java.util.Queue<Character> queue = new java.util.LinkedList<>();
+        // Create Deque
+        java.util.Deque<Character> deque = new java.util.LinkedList<>();
 
-        // Push to stack and enqueue to queue
+        // Insert characters into deque
         for (int i = 0; i < original.length(); i++) {
-            char ch = original.charAt(i);
-            stack.push(ch);      // LIFO
-            queue.add(ch);       // FIFO
+            deque.addLast(original.charAt(i));
         }
 
-        // Compare pop (stack) and dequeue (queue)
+        // Compare front and rear elements
         boolean isPalindrome = true;
 
-        while (!stack.isEmpty()) {
-            if (stack.pop() != queue.remove()) {
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
                 isPalindrome = false;
                 break;
             }
