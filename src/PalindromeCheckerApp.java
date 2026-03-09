c1376af7777f1123e224ed516920f423941b6a9c
itss-omkar <mishraomkarnarayan@gmail.com>
2026-03-09 15:13:39 +0530

    UC11:object oriented palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,34 +2,41 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Normalize the string
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        // create object
+        PalindromeService service = new PalindromeService();
 
-        boolean isPalindrome = true;
+        // call method
+        boolean result = service.checkPalindrome(input);
 
-        // Compare characters from both ends
-        for (int i = 0; i < normalized.length() / 2; i++) {
-
-            // Compare symmetric characters
-            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("The given string is NOT a Palindrome.");
-        }
+        if(result)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
 
         sc.close();
+    }class PalindromeService {
+
+        public boolean checkPalindrome(String input) {
+
+            int start = 0;
+            int end = input.length() - 1;
+
+            while(start < end) {
+                if(input.charAt(start) != input.charAt(end))
+                    return false;
+
+                start++;
+                end--;
+            }
+
+            return true;
+        }
     }
 }
