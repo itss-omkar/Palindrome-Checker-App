173fe3971bc4b6b702db7a9d722b7da55b5bbbaa
itss-omkar <mishraomkarnarayan@gmail.com>
2026-03-09 15:22:23 +0530

    UC13:performance comparision

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,50 +1,57 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-/* MAIN CLASS */
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Choose strategy at runtime
-        PalindromeStrategy strategy = new StackStrategy();
+        // -------- Algorithm 1 : Two Pointer --------
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerPalindrome(input);
+        long end1 = System.nanoTime();
 
-        boolean result = strategy.check(input);
+        // -------- Algorithm 2 : Stack --------
+        long start2 = System.nanoTime();
+        boolean result2 = stackPalindrome(input);
+        long end2 = System.nanoTime();
 
-        if(result)
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
+        System.out.println("\nTwo Pointer Result: " + result1);
+        System.out.println("Execution Time: " + (end1 - start1) + " ns");
+
+        System.out.println("\nStack Result: " + result2);
+        System.out.println("Execution Time: " + (end2 - start2) + " ns");
 
         sc.close();
     }
-}
 
-/* INTERFACE */
-interface PalindromeStrategy {
-    boolean check(String input);
-}
+    // Algorithm 1
+    public static boolean twoPointerPalindrome(String input) {
+        int start = 0;
+        int end = input.length() - 1;
 
-/* STACK STRATEGY CLASS */
-class StackStrategy implements PalindromeStrategy {
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
 
-    public boolean check(String input) {
-
+    // Algorithm 2
+    public static boolean stackPalindrome(String input) {
         Stack<Character> stack = new Stack<>();
 
-        // push characters into stack
-        for(char c : input.toCharArray()) {
+        for (char c : input.toCharArray()) {
             stack.push(c);
         }
 
-        // compare characters
-        for(char c : input.toCharArray()) {
-            if(c != stack.pop())
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop())
                 return false;
         }
 
